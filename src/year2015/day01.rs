ecd7a3f9c5997b2d91a619de0f3c4ec4ed1dfa33
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-07-29 12:06:15 +0100

    rewrite 2015 day1 part1

--- src/year2015/day01.rs
+++ src/year2015/day01.rs
@@ -3,16 +3,11 @@ pub fn parse(input: &str) -> String {
 }
 
 pub fn part1(input: &str) -> i32 {
-    let mut floor = 0;
-    for c in input.trim().chars() {
-        if c == '(' {
-            floor += 1;
-        } else {
-            floor -= 1;
-        }
-    }
-
-    floor
+    input
+        .trim()
+        .chars()
+        .map(|c| if c == '(' { 1 } else { -1 })
+        .sum()
 }
 
 pub fn part2(input: &str) -> usize {
