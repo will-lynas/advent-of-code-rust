9deccfeb37400ef93e0fdeca7173b5033e147a3b
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-07-29 16:15:25 +0100

    day4 test

--- tests/year2015/day04.rs
+++ tests/year2015/day04.rs
@@ -1,15 +1,14 @@
 use advent_of_code::year2015::day04 as solution;
 
-const EXAMPLE: &str = "Hello, World!";
+const EXAMPLE: &str = "abcdef";
 
 #[test]
 fn part1() {
     let input = solution::parse(EXAMPLE);
-    assert_eq!(solution::part1(&input), 13);
+    assert_eq!(solution::part1(&input), 609043);
 }
 
 #[test]
 fn part2() {
-    let input = solution::parse(EXAMPLE);
-    assert_eq!(solution::part2(&input), 13);
+    // No part 2 example
 }
