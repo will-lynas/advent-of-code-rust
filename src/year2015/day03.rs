2aeef727805f6001df8dda0b511f5627abdcc4a5
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-07-29 16:01:05 +0100

    use point class

--- src/year2015/day03.rs
+++ src/year2015/day03.rs
@@ -3,6 +3,14 @@ use gxhash::{
     HashSetExt,
 };
 
+use crate::utils::point::{
+    DOWN,
+    LEFT,
+    ORIGIN,
+    RIGHT,
+    UP,
+};
+
 type Input = String;
 
 pub fn parse(input: &str) -> Input {
@@ -10,15 +18,15 @@ pub fn parse(input: &str) -> Input {
 }
 
 pub fn part1(input: &Input) -> usize {
-    let mut position = (0, 0);
+    let mut position = ORIGIN;
     let mut visited = HashSet::new();
     visited.insert(position);
     for c in input.chars() {
         match c {
-            '^' => position.1 += 1,
-            'v' => position.1 -= 1,
-            '>' => position.0 += 1,
-            '<' => position.0 -= 1,
+            '^' => position += UP,
+            'v' => position += DOWN,
+            '>' => position += RIGHT,
+            '<' => position += LEFT,
             _ => unreachable!(),
         }
         visited.insert(position);
@@ -27,17 +35,17 @@ pub fn part1(input: &Input) -> usize {
 }
 
 pub fn part2(input: &Input) -> usize {
-    let mut positions = [(0, 0), (0, 0)];
+    let mut positions = [ORIGIN, ORIGIN];
     let mut visited = HashSet::new();
     visited.insert(positions[0]);
 
     for (i, c) in input.chars().enumerate() {
         let idx = i % 2;
         match c {
-            '^' => positions[idx].1 += 1,
-            'v' => positions[idx].1 -= 1,
-            '>' => positions[idx].0 += 1,
-            '<' => positions[idx].0 -= 1,
+            '^' => positions[idx] += UP,
+            'v' => positions[idx] += DOWN,
+            '>' => positions[idx] += RIGHT,
+            '<' => positions[idx] += LEFT,
             _ => unreachable!(),
         }
         visited.insert(positions[idx]);
