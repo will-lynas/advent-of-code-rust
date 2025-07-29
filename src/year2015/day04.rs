4a142beffc98b0a923132cfd6ec9e459a2686e84
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-07-29 16:38:35 +0100

    day4 part1

--- src/year2015/day04.rs
+++ src/year2015/day04.rs
@@ -1,3 +1,8 @@
+use md5::{
+    Digest,
+    Md5,
+};
+
 type Input = String;
 
 pub fn parse(input: &str) -> Input {
@@ -5,7 +10,18 @@ pub fn parse(input: &str) -> Input {
 }
 
 pub fn part1(input: &Input) -> usize {
-    input.len()
+    let bits = 4 * 5;
+    let mask = ((1u128 << bits) - 1) << (128 - bits);
+    let mut hasher = Md5::new();
+    for i in 0.. {
+        hasher.update(format!("{input}{i}"));
+        let hash: [u8; 16] = hasher.finalize_reset().into();
+        let value = u128::from_be_bytes(hash);
+        if value & mask == 0 {
+            return i;
+        }
+    }
+    unreachable!()
 }
 
 pub fn part2(input: &Input) -> usize {
