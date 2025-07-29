e39cdd4b4aa541c617861dd11e87535276846009
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-07-29 15:47:48 +0100

    day3

--- src/year2015/day03.rs
+++ src/year2015/day03.rs
@@ -1,3 +1,8 @@
+use gxhash::{
+    HashSet,
+    HashSetExt,
+};
+
 type Input = String;
 
 pub fn parse(input: &str) -> Input {
@@ -5,9 +10,50 @@ pub fn parse(input: &str) -> Input {
 }
 
 pub fn part1(input: &Input) -> usize {
-    input.len()
+    let mut x = 0;
+    let mut y = 0;
+    let mut visited = HashSet::new();
+    visited.insert((x, y));
+    for c in input.chars() {
+        match c {
+            '^' => y += 1,
+            'v' => y -= 1,
+            '>' => x += 1,
+            '<' => x -= 1,
+            _ => unreachable!(),
+        }
+        visited.insert((x, y));
+    }
+    visited.len()
 }
 
 pub fn part2(input: &Input) -> usize {
-    input.len()
+    let mut x1 = 0;
+    let mut y1 = 0;
+    let mut x2 = 0;
+    let mut y2 = 0;
+    let mut visited = HashSet::new();
+    visited.insert((x1, y1));
+    for (i, c) in input.chars().enumerate() {
+        if i % 2 == 0 {
+            match c {
+                '^' => y1 += 1,
+                'v' => y1 -= 1,
+                '>' => x1 += 1,
+                '<' => x1 -= 1,
+                _ => unreachable!(),
+            }
+            visited.insert((x1, y1));
+        } else {
+            match c {
+                '^' => y2 += 1,
+                'v' => y2 -= 1,
+                '>' => x2 += 1,
+                '<' => x2 -= 1,
+                _ => unreachable!(),
+            }
+            visited.insert((x2, y2));
+        }
+    }
+    visited.len()
 }
--- tests/year2015/day03.rs
+++ tests/year2015/day03.rs
@@ -1,15 +1,15 @@
 use advent_of_code::year2015::day03 as solution;
 
-const EXAMPLE: &str = "Hello, World!";
+const EXAMPLE: &str = "^v^v^v^v^v";
 
 #[test]
 fn part1() {
     let input = solution::parse(EXAMPLE);
-    assert_eq!(solution::part1(&input), 13);
+    assert_eq!(solution::part1(&input), 2);
 }
 
 #[test]
 fn part2() {
     let input = solution::parse(EXAMPLE);
-    assert_eq!(solution::part2(&input), 13);
+    assert_eq!(solution::part2(&input), 11);
 }
