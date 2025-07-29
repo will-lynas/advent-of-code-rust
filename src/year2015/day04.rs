055e047ca21e282a31b46ba862a03b5198761506
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-07-29 17:45:06 +0100

    avoid string allocations

--- src/year2015/day04.rs
+++ src/year2015/day04.rs
@@ -12,9 +12,15 @@ pub fn parse(input: &str) -> Input {
 fn find_leading_zeros(input: &str, n: usize) -> usize {
     let bits = 4 * n;
     let mask = ((1u128 << bits) - 1) << (128 - bits);
+
+    // Pre-create the hasher so we don't have to create it for each iteration
     let mut hasher = Md5::new();
+    // Pre-create the buffer to avoid allocations in each iteration
+    let mut buf = itoa::Buffer::new();
+
     for i in 0.. {
-        hasher.update(format!("{input}{i}"));
+        hasher.update(input);
+        hasher.update(buf.format(i));
         let hash: [u8; 16] = hasher.finalize_reset().into();
         let value = u128::from_be_bytes(hash);
         if value & mask == 0 {
