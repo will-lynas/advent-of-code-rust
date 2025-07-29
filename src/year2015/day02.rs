23bdc6f12402b61879ca809f7be5c5ed824a8a81
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-07-29 12:36:07 +0100

    day02 part1

--- src/year2015/day02.rs
+++ src/year2015/day02.rs
@@ -1,13 +1,31 @@
-type Input = String;
+type Input = Vec<Vec<usize>>;
 
 pub fn parse(input: &str) -> Input {
-    input.into()
+    input
+        .lines()
+        .map(|line| {
+            let mut dimensions = line
+                .split('x')
+                .map(str::parse)
+                .collect::<Result<Vec<_>, _>>()
+                .unwrap();
+            dimensions.sort_unstable();
+            dimensions
+        })
+        .collect()
 }
 
 pub fn part1(input: &Input) -> usize {
-    input.len()
+    input
+        .iter()
+        .map(|dimensions| {
+            (1 + 2) * dimensions[0] * dimensions[1]
+                + 2 * dimensions[1] * dimensions[2]
+                + 2 * dimensions[2] * dimensions[0]
+        })
+        .sum()
 }
 
-pub fn part2(input: &Input) -> usize {
-    input.len()
+pub fn part2(_input: &Input) -> usize {
+    0
 }
