ae5f59e253ff514960a92d71bc7f6981308231ce
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-07-29 18:59:17 +0100

    solve part1

--- src/year2015/day05.rs
+++ src/year2015/day05.rs
@@ -1,11 +1,23 @@
-type Input = String;
+type Input = Vec<String>;
+
+const VOWELS: &[char] = &['a', 'e', 'i', 'o', 'u'];
+const FORBIDDEN: &[&str] = &["ab", "cd", "pq", "xy"];
 
 pub fn parse(input: &str) -> Input {
-    input.into()
+    input.lines().map(Into::into).collect()
 }
 
 pub fn part1(input: &Input) -> usize {
-    input.len()
+    input
+        .iter()
+        .filter(|s| s.chars().filter(|c| VOWELS.contains(c)).count() >= 3)
+        .filter(|s| {
+            ('a'..='z')
+                .map(|c| c.to_string().repeat(2))
+                .any(|f| s.contains(&f.to_string()))
+        })
+        .filter(|s| !FORBIDDEN.iter().any(|f| s.contains(f)))
+        .count()
 }
 
 pub fn part2(input: &Input) -> usize {
