26324e7b5db88169a62e6c538ea63b0baff4be38
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-07-29 18:47:40 +0100

    day5 tests

--- tests/year2015/day05.rs
+++ tests/year2015/day05.rs
@@ -1,15 +1,34 @@
 use advent_of_code::year2015::day05 as solution;
 
-const EXAMPLE: &str = "Hello, World!";
-
 #[test]
 fn part1() {
-    let input = solution::parse(EXAMPLE);
-    assert_eq!(solution::part1(&input), 13);
+    let input = solution::parse("ugknbfddgicrmopn");
+    assert_eq!(solution::part1(&input), 1);
+
+    let input = solution::parse("aaa");
+    assert_eq!(solution::part1(&input), 1);
+
+    let input = solution::parse("jchzalrnumimnmhp");
+    assert_eq!(solution::part1(&input), 0);
+
+    let input = solution::parse("haegwjzuvuyypxyu");
+    assert_eq!(solution::part1(&input), 0);
+
+    let input = solution::parse("dvszwmarrgswjxmb");
+    assert_eq!(solution::part1(&input), 0);
 }
 
 #[test]
 fn part2() {
-    let input = solution::parse(EXAMPLE);
-    assert_eq!(solution::part2(&input), 13);
+    let input = solution::parse("qjhvhtzxzqqjkmpb");
+    assert_eq!(solution::part2(&input), 1);
+
+    let input = solution::parse("xxyxx");
+    assert_eq!(solution::part2(&input), 1);
+
+    let input = solution::parse("uurcxstgmygtbstg");
+    assert_eq!(solution::part2(&input), 0);
+
+    let input = solution::parse("ieodomkazucvgmuy");
+    assert_eq!(solution::part2(&input), 0);
 }
