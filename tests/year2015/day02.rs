2937fcdc557323c5bb5e3f74d02d241b901f5ada
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-07-29 12:19:03 +0100

    add day02 tests

--- tests/year2015/day02.rs
+++ tests/year2015/day02.rs
@@ -1,15 +1,15 @@
 use advent_of_code::year2015::day02 as solution;
 
-const EXAMPLE: &str = "Hello, World!";
+const EXAMPLE: &str = "2x3x4";
 
 #[test]
 fn part1() {
     let input = solution::parse(EXAMPLE);
-    assert_eq!(solution::part1(&input), 13);
+    assert_eq!(solution::part1(&input), 58);
 }
 
 #[test]
 fn part2() {
     let input = solution::parse(EXAMPLE);
-    assert_eq!(solution::part2(&input), 13);
+    assert_eq!(solution::part2(&input), 34);
 }
