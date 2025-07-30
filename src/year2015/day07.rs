319fe2a2a6474376e583e5b8fd7fd7d7bbf4896a
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-07-30 01:23:43 +0100

    solve day7 part1

--- src/year2015/day07.rs
+++ src/year2015/day07.rs
@@ -1,13 +1,108 @@
-type Input = String;
+use gxhash::{
+    HashMap,
+    HashMapExt,
+};
 
-pub fn parse(input: &str) -> Input {
-    input.into()
+type Connections = HashMap<String, Operator>;
+
+#[derive(Debug)]
+pub enum Operand {
+    Literal(u16),
+    Wire(String),
 }
 
-pub fn part1(input: &Input) -> usize {
-    input.len()
+pub enum Operator {
+    And(Operand, Operand),
+    Or(Operand, Operand),
+    LShift(Operand, Operand),
+    RShift(Operand, Operand),
+    Not(Operand),
+    Identity(Operand),
 }
 
-pub fn part2(input: &Input) -> usize {
-    input.len()
+impl Operand {
+    fn parse(operand: &str) -> Self {
+        match operand.parse::<u16>() {
+            Ok(literal) => Operand::Literal(literal),
+            Err(_) => Operand::Wire(operand.into()),
+        }
+    }
+}
+
+pub fn parse(input: &str) -> Connections {
+    let mut connections = HashMap::new();
+    for line in input.lines() {
+        let (expression, wire) = line.split_once(" -> ").unwrap();
+        let expression: Vec<_> = expression.split_whitespace().collect();
+        let operator = match expression.len() {
+            1 => Operator::Identity(Operand::parse(expression[0])),
+            2 => Operator::Not(Operand::parse(expression[1])),
+            3 => {
+                let lhs = Operand::parse(expression[0]);
+                let rhs = Operand::parse(expression[2]);
+                #[allow(clippy::match_on_vec_items)]
+                match expression[1] {
+                    "AND" => Operator::And(lhs, rhs),
+                    "OR" => Operator::Or(lhs, rhs),
+                    "LSHIFT" => Operator::LShift(lhs, rhs),
+                    "RSHIFT" => Operator::RShift(lhs, rhs),
+                    _ => unreachable!(),
+                }
+            }
+            _ => unreachable!(),
+        };
+        connections.insert(wire.into(), operator);
+    }
+    connections
+}
+
+fn evaluate_operand(
+    cache: &mut HashMap<String, u16>,
+    connections: &Connections,
+    operand: &Operand,
+) -> u16 {
+    match operand {
+        Operand::Literal(value) => *value,
+        Operand::Wire(wire) => {
+            if let Some(value) = cache.get(wire) {
+                *value
+            } else {
+                let value = evaluate_operator(cache, connections, connections.get(wire).unwrap());
+                cache.insert(wire.clone(), value);
+                value
+            }
+        }
+    }
+}
+
+fn evaluate_operator(
+    cache: &mut HashMap<String, u16>,
+    connections: &Connections,
+    operator: &Operator,
+) -> u16 {
+    match operator {
+        Operator::Identity(operand) => evaluate_operand(cache, connections, operand),
+        Operator::Not(operand) => !evaluate_operand(cache, connections, operand),
+        Operator::And(lhs, rhs) => {
+            evaluate_operand(cache, connections, lhs) & evaluate_operand(cache, connections, rhs)
+        }
+        Operator::Or(lhs, rhs) => {
+            evaluate_operand(cache, connections, lhs) | evaluate_operand(cache, connections, rhs)
+        }
+        Operator::LShift(lhs, rhs) => {
+            evaluate_operand(cache, connections, lhs) << evaluate_operand(cache, connections, rhs)
+        }
+        Operator::RShift(lhs, rhs) => {
+            evaluate_operand(cache, connections, lhs) >> evaluate_operand(cache, connections, rhs)
+        }
+    }
+}
+
+pub fn part1(connections: &Connections) -> u16 {
+    let mut cache = HashMap::new();
+    evaluate_operand(&mut cache, connections, &Operand::Wire("a".into()))
+}
+
+pub fn part2(connections: &Connections) -> usize {
+    connections.len()
 }
