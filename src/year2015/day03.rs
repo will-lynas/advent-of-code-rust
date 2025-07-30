6068912696763c3de81ed09c5bd0c6c530846c45
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-07-30 02:13:18 +0100

    clean up day3

--- src/year2015/day03.rs
+++ src/year2015/day03.rs
@@ -5,43 +5,39 @@ use gxhash::{
 
 use crate::utils::point::Point;
 
-type Input = String;
-
-pub fn parse(input: &str) -> Input {
-    input.into()
+pub fn parse(input: &str) -> Vec<Point> {
+    input
+        .chars()
+        .map(|c| match c {
+            '^' => Point::UP,
+            'v' => Point::DOWN,
+            '>' => Point::RIGHT,
+            '<' => Point::LEFT,
+            _ => unreachable!(),
+        })
+        .collect()
 }
 
-pub fn part1(input: &Input) -> usize {
+pub fn part1(input: &[Point]) -> usize {
     let mut position = Point::ORIGIN;
     let mut visited = HashSet::new();
     visited.insert(position);
-    for c in input.chars() {
-        match c {
-            '^' => position += Point::UP,
-            'v' => position += Point::DOWN,
-            '>' => position += Point::RIGHT,
-            '<' => position += Point::LEFT,
-            _ => unreachable!(),
-        }
+
+    for &delta in input {
+        position += delta;
         visited.insert(position);
     }
     visited.len()
 }
 
-pub fn part2(input: &Input) -> usize {
+pub fn part2(input: &[Point]) -> usize {
     let mut positions = [Point::ORIGIN, Point::ORIGIN];
     let mut visited = HashSet::new();
     visited.insert(positions[0]);
 
-    for (i, c) in input.chars().enumerate() {
+    for (i, &delta) in input.iter().enumerate() {
         let idx = i % 2;
-        match c {
-            '^' => positions[idx] += Point::UP,
-            'v' => positions[idx] += Point::DOWN,
-            '>' => positions[idx] += Point::RIGHT,
-            '<' => positions[idx] += Point::LEFT,
-            _ => unreachable!(),
-        }
+        positions[idx] += delta;
         visited.insert(positions[idx]);
     }
     visited.len()
