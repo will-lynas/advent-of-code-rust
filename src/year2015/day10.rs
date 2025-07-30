aa5e561febdcde0f70252dcdb4ff4cf68cb971cd
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-07-30 20:53:10 +0100

    solve part1

--- src/year2015/day10.rs
+++ src/year2015/day10.rs
@@ -1,13 +1,33 @@
-type Input = String;
-
-pub fn parse(input: &str) -> Input {
-    input.into()
+pub fn parse(input: &str) -> String {
+    input.to_string()
 }
 
-pub fn part1(input: &Input) -> usize {
-    input.len()
+fn transform(input: String) -> String {
+    let mut result = String::new();
+    let mut chars = input.chars();
+    let mut count = 1;
+    let mut current = chars.next().unwrap();
+    for c in chars {
+        if c == current {
+            count += 1;
+        } else {
+            result.push_str(&count.to_string());
+            result.push(current);
+            count = 1;
+            current = c;
+        }
+    }
+    result.push_str(&count.to_string());
+    result.push(current);
+    result
 }
 
-pub fn part2(input: &Input) -> usize {
+pub fn part1(input: &str) -> usize {
+    (0..40)
+        .fold(input.to_string(), |acc, _| transform(acc))
+        .len()
+}
+
+pub fn part2(input: &str) -> usize {
     input.len()
 }
