56a180341db01398eb10f061e6b4fefa523b352e
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-07-30 17:49:02 +0100

    solve part2

--- src/year2015/day09.rs
+++ src/year2015/day09.rs
@@ -57,6 +57,35 @@ pub fn part1(graph: &Graph) -> usize {
         .unwrap()
 }
 
-pub fn part2(input: &Graph) -> usize {
-    input.len()
+fn solve2(graph: &Graph, remaining: &HashSet<&str>, distance: usize, current_place: &str) -> usize {
+    if remaining.is_empty() {
+        return distance;
+    }
+    remaining
+        .iter()
+        .map(|&new_place| {
+            let mut new_remaining = remaining.clone();
+            new_remaining.remove(new_place);
+            solve2(
+                graph,
+                &new_remaining,
+                distance + graph[current_place][new_place],
+                new_place,
+            )
+        })
+        .max()
+        .unwrap()
+}
+
+pub fn part2(graph: &Graph) -> usize {
+    let places: HashSet<&str> = graph.keys().map(String::as_str).collect();
+    places
+        .iter()
+        .map(|place| {
+            let mut new_places = places.clone();
+            new_places.remove(place);
+            solve2(graph, &new_places, 0, place)
+        })
+        .max()
+        .unwrap()
 }
--- tests/year2015/day09.rs
+++ tests/year2015/day09.rs
@@ -13,5 +13,5 @@ fn part1() {
 #[test]
 fn part2() {
     let input = solution::parse(EXAMPLE);
-    assert_eq!(solution::part2(&input), 13);
+    assert_eq!(solution::part2(&input), 982);
 }
