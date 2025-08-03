405502764f2989a06f199d5281ea8ccc5e2d5c74
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-08-03 23:16:28 +0100

    use vec instead of hashmap

--- src/year2015/day13.rs
+++ src/year2015/day13.rs
@@ -5,12 +5,10 @@ use gxhash::{
 use itertools::Itertools;
 use regex::Regex;
 
-type Pairs = HashMap<String, HashMap<String, i32>>;
-
-pub fn parse(input: &str) -> Pairs {
+pub fn parse(input: &str) -> Vec<Vec<i32>> {
     let re = Regex::new(r"(\w+) would (gain|lose) (\d+) happiness units by sitting next to (\w+).")
         .unwrap();
-    let mut map: Pairs = HashMap::new();
+    let mut map: HashMap<String, HashMap<String, i32>> = HashMap::new();
     for line in input.lines() {
         let caps = re.captures(line).unwrap();
         let name1 = caps[1].to_string();
@@ -19,37 +17,42 @@ pub fn parse(input: &str) -> Pairs {
         let name2 = caps[4].to_string();
         map.entry(name1).or_default().insert(name2, mult * num);
     }
-    map
+    let names_map: HashMap<String, usize> = map
+        .keys()
+        .enumerate()
+        .map(|(i, k)| (k.clone(), i))
+        .collect();
+    let mut pairs: Vec<Vec<i32>> = vec![vec![0; names_map.len()]; names_map.len()];
+    for (name1, map) in map {
+        for (name2, value) in map {
+            pairs[names_map[&name1]][names_map[&name2]] = value;
+        }
+    }
+    pairs
 }
 
-pub fn part1(input: &Pairs) -> i32 {
-    let names: Vec<_> = input.keys().cloned().collect();
-    names
-        .iter()
-        .skip(1)
-        .permutations(names.len() - 1)
+pub fn part1(pairs: &[Vec<i32>]) -> i32 {
+    let n = pairs.len();
+    (1..n)
+        .permutations(n - 1)
         .map(|perm| {
-            let mut perm = perm;
-            perm.push(&names[0]);
             perm.iter()
-                .circular_tuple_windows()
-                .map(|(&a, &b)| input[a][b] + input[b][a])
-                .sum()
+                .tuple_windows()
+                .map(|(&a, &b)| pairs[a][b] + pairs[b][a])
+                .sum::<i32>()
+                + (pairs[0][perm[0]] + pairs[perm[0]][0])
+                + (pairs[perm[n - 2]][0] + pairs[0][perm[n - 2]])
         })
         .max()
         .unwrap()
 }
 
-pub fn part2(input: &Pairs) -> i32 {
-    let mut input = input.clone();
-    let previous_names: Vec<_> = input.keys().cloned().collect();
-    for name in previous_names {
-        input
-            .entry(name.clone())
-            .or_default()
-            .insert("me".to_string(), 0);
-        input.entry("me".to_string()).or_default().insert(name, 0);
+pub fn part2(pairs: &[Vec<i32>]) -> i32 {
+    let mut pairs = pairs.to_vec();
+    let n = pairs.len();
+    for row in &mut pairs {
+        row.push(0);
     }
-
-    part1(&input)
+    pairs.push(vec![0; n + 1]);
+    part1(&pairs)
 }
