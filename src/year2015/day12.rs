78000261947a53ab753c92a693aaeda4cadfd840
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-08-03 16:58:19 +0100

    solve part2

--- src/year2015/day12.rs
+++ src/year2015/day12.rs
@@ -10,10 +10,21 @@ pub fn part1(value: &Value) -> i64 {
         Value::Array(arr) => arr.iter().map(part1).sum(),
         Value::Number(num) => num.as_i64().unwrap(),
         Value::String(_) => 0,
-        _ => unreachable!(),
+        Value::Null | Value::Bool(_) => unreachable!(),
     }
 }
 
-pub fn part2(_input: &Value) -> usize {
-    0
+pub fn solve2(value: &Value) -> Option<i64> {
+    match value {
+        Value::Object(obj) => Some(obj.values().map(solve2).sum::<Option<i64>>().unwrap_or(0)),
+        Value::Array(arr) => Some(arr.iter().map(|x| solve2(x).unwrap_or(0)).sum()),
+        Value::Number(num) => Some(num.as_i64().unwrap()),
+        Value::String(s) if s == "red" => None,
+        Value::String(_) => Some(0),
+        Value::Null | Value::Bool(_) => unreachable!(),
+    }
+}
+
+pub fn part2(input: &Value) -> i64 {
+    solve2(input).unwrap_or(0)
 }
