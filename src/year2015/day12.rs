a1d666a4ef0657083408ba63834b2664f4b31dea
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-08-03 16:38:51 +0100

    day12 part1

--- src/year2015/day12.rs
+++ src/year2015/day12.rs
@@ -1,13 +1,19 @@
-type Input = String;
+use serde_json::Value;
 
-pub fn parse(input: &str) -> Input {
-    input.into()
+pub fn parse(input: &str) -> Value {
+    serde_json::from_str(input).unwrap()
 }
 
-pub fn part1(input: &Input) -> usize {
-    input.len()
+pub fn part1(value: &Value) -> i64 {
+    match value {
+        Value::Object(obj) => obj.values().map(part1).sum(),
+        Value::Array(arr) => arr.iter().map(part1).sum(),
+        Value::Number(num) => num.as_i64().unwrap(),
+        Value::String(_) => 0,
+        _ => unreachable!(),
+    }
 }
 
-pub fn part2(input: &Input) -> usize {
-    input.len()
+pub fn part2(_input: &Value) -> usize {
+    0
 }
