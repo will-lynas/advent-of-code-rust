b5b622093a78b4452db5827a51c415311b36a1cf
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-08-03 19:12:04 +0100

    add day13 tests

--- tests/year2015/day13.rs
+++ tests/year2015/day13.rs
@@ -1,15 +1,27 @@
 use advent_of_code::year2015::day13 as solution;
 
-const EXAMPLE: &str = "Hello, World!";
+const EXAMPLE: &str = "
+Alice would gain 54 happiness units by sitting next to Bob.
+Alice would lose 79 happiness units by sitting next to Carol.
+Alice would lose 2 happiness units by sitting next to David.
+Bob would gain 83 happiness units by sitting next to Alice.
+Bob would lose 7 happiness units by sitting next to Carol.
+Bob would lose 63 happiness units by sitting next to David.
+Carol would lose 62 happiness units by sitting next to Alice.
+Carol would gain 60 happiness units by sitting next to Bob.
+Carol would gain 55 happiness units by sitting next to David.
+David would gain 46 happiness units by sitting next to Alice.
+David would lose 7 happiness units by sitting next to Bob.
+David would gain 41 happiness units by sitting next to Carol.";
 
 #[test]
 fn part1() {
     let input = solution::parse(EXAMPLE);
-    assert_eq!(solution::part1(&input), 13);
+    assert_eq!(solution::part1(&input), 330);
 }
 
 #[test]
 fn part2() {
     let input = solution::parse(EXAMPLE);
-    assert_eq!(solution::part2(&input), 13);
+    assert_eq!(solution::part2(&input), 0);
 }
