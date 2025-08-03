7351a9175b365d27b5174507c31e6466c828c963
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-08-03 15:12:46 +0100

    day11 part1

--- src/year2015/day11.rs
+++ src/year2015/day11.rs
@@ -1,13 +1,50 @@
-type Input = String;
-
-pub fn parse(input: &str) -> Input {
-    input.into()
+pub fn parse(input: &str) -> [u8; 8] {
+    input.as_bytes().try_into().unwrap()
 }
 
-pub fn part1(input: &Input) -> usize {
-    input.len()
+pub fn part1(password: &[u8; 8]) -> String {
+    let mut password = password.to_vec();
+    loop {
+        // increment
+        let mut i = 7;
+        loop {
+            password[i] += 1;
+            if password[i] > b'z' {
+                password[i] = b'a';
+                i -= 1;
+            } else {
+                break;
+            }
+        }
+
+        if !password
+            .windows(3)
+            .any(|w| w[0] + 1 == w[1] && w[1] + 1 == w[2])
+        {
+            continue;
+        }
+
+        if password
+            .iter()
+            .any(|&c| c == b'i' || c == b'o' || c == b'l')
+        {
+            continue;
+        }
+
+        if (b'a'..=b'z')
+            .filter(|&c| password.windows(2).any(|w| w[0] == c && w[1] == c))
+            .count()
+            < 2
+        {
+            continue;
+        }
+
+        break;
+    }
+
+    String::from_utf8(password).unwrap()
 }
 
-pub fn part2(input: &Input) -> usize {
+pub fn part2(input: &[u8; 8]) -> usize {
     input.len()
 }
