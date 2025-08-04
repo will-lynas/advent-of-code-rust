2117e56177fa0f2a8a25a191562c0338a41e9278
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-08-05 00:16:29 +0100

    remove trim

--- tests/year2015/day15.rs
+++ tests/year2015/day15.rs
@@ -3,7 +3,7 @@ use advent_of_code::year2015::day15 as solution;
 // dummy ingredients are used so that the length of the input can be
 // hardcoded as 4.
 // these ingredients are all negative, so they will never be used.
-const EXAMPLE: &str = "
+const EXAMPLE: &str = "\
 Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8
 Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3
 Dummy1: capacity -100, durability -100, flavor -100, texture -100, calories 0
@@ -11,12 +11,12 @@ Dummy2: capacity -100, durability -100, flavor -100, texture -100, calories 0";
 
 #[test]
 fn part1() {
-    let input = solution::parse(EXAMPLE.trim());
+    let input = solution::parse(EXAMPLE);
     assert_eq!(solution::part1(&input), 62842880);
 }
 
 #[test]
 fn part2() {
-    let input = solution::parse(EXAMPLE.trim());
+    let input = solution::parse(EXAMPLE);
     assert_eq!(solution::part2(&input), 0);
 }
