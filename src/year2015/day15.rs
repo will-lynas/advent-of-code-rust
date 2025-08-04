8c459aa191ce62caedb9dae810b94a4710c83c5e
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-08-05 00:01:01 +0100

    use a struct

--- src/year2015/day15.rs
+++ src/year2015/day15.rs
@@ -1,37 +1,43 @@
 use regex::Regex;
 
-type Input = Vec<(i64, i64, i64, i64, i64)>;
+pub struct Ingredient {
+    capacity: i64,
+    durability: i64,
+    flavor: i64,
+    texture: i64,
+    calories: i64,
+}
 
-pub fn parse(input: &str) -> Input {
+pub fn parse(input: &str) -> Vec<Ingredient> {
     let re = Regex::new(r" (-?\d+).* (-?\d+).* (-?\d+).* (-?\d+).* (-?\d+)").unwrap();
     input
         .lines()
         .map(|line| {
             let caps = re.captures(line).unwrap();
-            (
-                caps[1].parse().unwrap(),
-                caps[2].parse().unwrap(),
-                caps[3].parse().unwrap(),
-                caps[4].parse().unwrap(),
-                caps[5].parse().unwrap(),
-            )
+            Ingredient {
+                capacity: caps[1].parse().unwrap(),
+                durability: caps[2].parse().unwrap(),
+                flavor: caps[3].parse().unwrap(),
+                texture: caps[4].parse().unwrap(),
+                calories: caps[5].parse().unwrap(),
+            }
         })
         .collect()
 }
 
-pub fn part1(ingredients: &Input) -> i64 {
+pub fn part1(ingredients: &[Ingredient]) -> i64 {
     let mut max_score = 0;
     for i in 0..100 {
         let j = 100 - i;
-        let mut score = (i * ingredients[0].0 + j * ingredients[1].0).max(0);
-        score *= (i * ingredients[0].1 + j * ingredients[1].1).max(0);
-        score *= (i * ingredients[0].2 + j * ingredients[1].2).max(0);
-        score *= (i * ingredients[0].3 + j * ingredients[1].3).max(0);
+        let mut score = (i * ingredients[0].capacity + j * ingredients[1].capacity).max(0);
+        score *= (i * ingredients[0].durability + j * ingredients[1].durability).max(0);
+        score *= (i * ingredients[0].flavor + j * ingredients[1].flavor).max(0);
+        score *= (i * ingredients[0].texture + j * ingredients[1].texture).max(0);
         max_score = max_score.max(score);
     }
     max_score
 }
 
-pub fn part2(input: &Input) -> usize {
-    input.len()
+pub fn part2(ingredients: &[Ingredient]) -> i64 {
+    todo!()
 }
