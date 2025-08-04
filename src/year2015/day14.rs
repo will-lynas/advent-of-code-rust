ca7c21e4709fdf723a9f811d6ee4304004c6f963
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-08-04 12:05:39 +0100

    day14 part1

--- src/year2015/day14.rs
+++ src/year2015/day14.rs
@@ -1,11 +1,34 @@
-type Input = String;
+use regex::Regex;
+
+type Input = Vec<(u32, u32, u32)>;
 
 pub fn parse(input: &str) -> Input {
-    input.into()
+    let re = Regex::new(r"(\d+) .* (\d+) .* (\d+)").unwrap();
+    input
+        .lines()
+        .map(|line| {
+            let caps = re.captures(line).unwrap();
+            (
+                caps[1].parse().unwrap(),
+                caps[2].parse().unwrap(),
+                caps[3].parse().unwrap(),
+            )
+        })
+        .collect()
 }
 
-pub fn part1(input: &Input) -> usize {
-    input.len()
+pub fn part1(input: &Input) -> u32 {
+    let t = 2503;
+    input
+        .iter()
+        .map(|(speed, fly, rest)| {
+            let cycle = fly + rest;
+            let full_cycles = t / cycle;
+            let remaining = t % cycle;
+            full_cycles * speed * fly + speed * remaining.min(*fly)
+        })
+        .max()
+        .unwrap()
 }
 
 pub fn part2(input: &Input) -> usize {
