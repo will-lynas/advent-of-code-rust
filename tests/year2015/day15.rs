ff52d474cbb92eb595109b358cc7c56f1c611fa1
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-08-04 18:44:42 +0100

    day15 tests

--- tests/year2015/day15.rs
+++ tests/year2015/day15.rs
@@ -1,15 +1,17 @@
 use advent_of_code::year2015::day15 as solution;
 
-const EXAMPLE: &str = "Hello, World!";
+const EXAMPLE: &str = "
+Butterscotch: capacity -1, durability -2, flavor 6, texture 3, calories 8
+Cinnamon: capacity 2, durability 3, flavor -2, texture -1, calories 3";
 
 #[test]
 fn part1() {
-    let input = solution::parse(EXAMPLE);
-    assert_eq!(solution::part1(&input), 13);
+    let input = solution::parse(EXAMPLE.trim());
+    assert_eq!(solution::part1(&input), 62842880);
 }
 
 #[test]
 fn part2() {
-    let input = solution::parse(EXAMPLE);
-    assert_eq!(solution::part2(&input), 13);
+    let input = solution::parse(EXAMPLE.trim());
+    assert_eq!(solution::part2(&input), 0);
 }
