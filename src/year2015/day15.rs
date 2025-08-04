f529963221278a826a37d8dcf1a34e12256ac02c
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-08-05 00:21:37 +0100

    solve part2

--- src/year2015/day15.rs
+++ src/year2015/day15.rs
@@ -8,9 +8,9 @@ pub struct Ingredient {
     calories: i64,
 }
 
-pub fn parse(input: &str) -> Vec<Ingredient> {
+pub fn parse(input: &str) -> (i64, i64) {
     let re = Regex::new(r" (-?\d+).* (-?\d+).* (-?\d+).* (-?\d+).* (-?\d+)").unwrap();
-    input
+    let r: Vec<Ingredient> = input
         .lines()
         .map(|line| {
             let caps = re.captures(line).unwrap();
@@ -22,11 +22,11 @@ pub fn parse(input: &str) -> Vec<Ingredient> {
                 calories: caps[5].parse().unwrap(),
             }
         })
-        .collect()
-}
+        .collect();
 
-pub fn part1(r: &[Ingredient]) -> i64 {
     let mut max_score = 0;
+    let mut max_score_500_calories = 0;
+
     for i in 0..=100 {
         for j in 0..=100 - i {
             for k in 0..=100 - i - j {
@@ -45,13 +45,23 @@ pub fn part1(r: &[Ingredient]) -> i64 {
                         .max(0)
                     * (i * r[0].texture + j * r[1].texture + k * r[2].texture + l * r[3].texture)
                         .max(0);
+
                 max_score = max_score.max(score);
+                if i * r[0].calories + j * r[1].calories + k * r[2].calories + l * r[3].calories
+                    == 500
+                {
+                    max_score_500_calories = max_score_500_calories.max(score);
+                }
             }
         }
     }
-    max_score
+    (max_score, max_score_500_calories)
 }
 
-pub fn part2(ingredients: &[Ingredient]) -> i64 {
-    todo!()
+pub fn part1(&(ans, _): &(i64, i64)) -> i64 {
+    ans
+}
+
+pub fn part2(&(_, ans): &(i64, i64)) -> i64 {
+    ans
 }
--- tests/year2015/day15.rs
+++ tests/year2015/day15.rs
@@ -18,5 +18,5 @@ fn part1() {
 #[test]
 fn part2() {
     let input = solution::parse(EXAMPLE);
-    assert_eq!(solution::part2(&input), 0);
+    assert_eq!(solution::part2(&input), 57600000);
 }
