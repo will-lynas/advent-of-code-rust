357698b46247b1c9108c0634ec8e5cbcb37e20e0
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-08-06 00:04:42 +0100

    90% speed buff day5 part1

--- src/year2015/day05.rs
+++ src/year2015/day05.rs
@@ -17,11 +17,7 @@ pub fn part1(input: &Input) -> usize {
     input
         .iter()
         .filter(|s| s.chars().filter(|c| VOWELS.contains(c)).count() >= 3)
-        .filter(|s| {
-            ('a'..='z')
-                .map(|c| c.to_string().repeat(2))
-                .any(|f| s.contains(&f.to_string()))
-        })
+        .filter(|s| s.chars().tuple_windows().any(|(a, b)| a == b))
         .filter(|s| !FORBIDDEN.iter().any(|f| s.contains(f)))
         .count()
 }
