31dd2d836bbebdc59a5fbdc7610d799731c6b0cb
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-08-08 00:24:33 +0100

    unify parts

--- src/year2015/day17.rs
+++ src/year2015/day17.rs
@@ -1,24 +1,9 @@
 pub fn parse(input: &str) -> Vec<usize> {
-    input.lines().map(|line| line.parse().unwrap()).collect()
+    let containers: Vec<_> = input.lines().map(|line| line.parse().unwrap()).collect();
+    solve(&containers)
 }
 
-pub fn part1(containers: &[usize]) -> usize {
-    let mut count = 0;
-    for mask in 0..(1 << containers.len()) {
-        let mut sum = 0;
-        for (i, container) in containers.iter().enumerate() {
-            if mask & (1 << i) != 0 {
-                sum += container;
-            }
-        }
-        if sum == 150 {
-            count += 1;
-        }
-    }
-    count
-}
-
-pub fn part2(containers: &[usize]) -> usize {
+pub fn solve(containers: &[usize]) -> Vec<usize> {
     let mut counts: Vec<usize> = vec![0; containers.len()];
     for mask in 0..(1 << containers.len()) {
         let mut sum = 0;
@@ -33,6 +18,14 @@ pub fn part2(containers: &[usize]) -> usize {
             counts[n] += 1;
         }
     }
+    counts
+}
+
+pub fn part1(counts: &[usize]) -> usize {
+    counts.iter().sum()
+}
+
+pub fn part2(counts: &[usize]) -> usize {
     let min = counts.iter().position(|&c| c > 0).unwrap();
     counts[min]
 }
