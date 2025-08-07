7cfffdb6fcfaa218894ce792f5b30aa64ea18fee
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-08-08 00:14:44 +0100

    solve part1

--- src/year2015/day17.rs
+++ src/year2015/day17.rs
@@ -1,13 +1,23 @@
-type Input = String;
-
-pub fn parse(input: &str) -> Input {
-    input.into()
+pub fn parse(input: &str) -> Vec<usize> {
+    input.lines().map(|line| line.parse().unwrap()).collect()
 }
 
-pub fn part1(input: &Input) -> usize {
-    input.len()
+pub fn part1(containers: &[usize]) -> usize {
+    let mut count = 0;
+    for mask in 0..(1 << containers.len()) {
+        let mut sum = 0;
+        for (i, container) in containers.iter().enumerate() {
+            if mask & (1 << i) != 0 {
+                sum += container;
+            }
+        }
+        if sum == 150 {
+            count += 1;
+        }
+    }
+    count
 }
 
-pub fn part2(input: &Input) -> usize {
+pub fn part2(input: &[usize]) -> usize {
     input.len()
 }
