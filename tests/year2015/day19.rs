c74b892734a5ee31b5a973a24e3a7a60378bc47f
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-08-08 00:32:00 +0100

    day19 test

--- tests/year2015/day19.rs
+++ tests/year2015/day19.rs
@@ -1,11 +1,16 @@
 use advent_of_code::year2015::day19 as solution;
 
-const EXAMPLE: &str = "Hello, World!";
+const EXAMPLE: &str = "\
+H => HO
+H => OH
+O => HH
+
+HOH";
 
 #[test]
 fn part1() {
     let input = solution::parse(EXAMPLE);
-    assert_eq!(solution::part1(&input), 13);
+    assert_eq!(solution::part1(&input), 4);
 }
 
 #[test]
