4ad6121c0ce1dee949c08c51521f80c60157cb6e
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-08-08 01:36:04 +0100

    fix test again

--- tests/year2015/day19.rs
+++ tests/year2015/day19.rs
@@ -1,14 +1,15 @@
 use advent_of_code::year2015::day19 as solution;
 
-#[test]
-fn part1() {
-    let example = "\
+const EXAMPLE: &str = "\
 H => HO
 H => OH
 O => HH
 
 HOH";
-    let input = solution::parse(example);
+
+#[test]
+fn part1() {
+    let input = solution::parse(EXAMPLE);
     assert_eq!(solution::part1(&input), 4);
 }
 
