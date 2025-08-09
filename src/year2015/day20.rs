6186dabb94029f3e45f02da6ced94ebc236f87fe
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-08-09 15:35:25 +0100

    move stuff out of parse

--- src/year2015/day20.rs
+++ src/year2015/day20.rs
@@ -1,13 +1,13 @@
 use divisors::get_divisors;
 
 pub fn parse(n: &str) -> u32 {
-    let n: u32 = n.parse().unwrap();
-    // -1 because get_divisors excludes 1
-    // and every house has 1 as a divisor
-    n / 10 - 1
+    n.parse().unwrap()
 }
 
 pub fn part1(&n: &u32) -> u32 {
+    // -1 because get_divisors excludes 1
+    // and every house has 1 as a divisor
+    let n = n / 10 - 1;
     #[allow(clippy::maybe_infinite_iter)]
     (1..)
         .find(|&i| get_divisors(i).iter().sum::<u32>() + i >= n)
