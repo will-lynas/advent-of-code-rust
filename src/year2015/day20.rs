90e8054c14e3dec5e1188edf72d83b96bf136fbc
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-08-09 15:33:09 +0100

    solve day20 p1

--- src/year2015/day20.rs
+++ src/year2015/day20.rs
@@ -1,13 +1,19 @@
-type Input = String;
+use divisors::get_divisors;
 
-pub fn parse(input: &str) -> Input {
-    input.into()
+pub fn parse(n: &str) -> u32 {
+    let n: u32 = n.parse().unwrap();
+    // -1 because get_divisors excludes 1
+    // and every house has 1 as a divisor
+    n / 10 - 1
 }
 
-pub fn part1(input: &Input) -> usize {
-    input.len()
+pub fn part1(&n: &u32) -> u32 {
+    #[allow(clippy::maybe_infinite_iter)]
+    (1..)
+        .find(|&i| get_divisors(i).iter().sum::<u32>() + i >= n)
+        .unwrap()
 }
 
-pub fn part2(input: &Input) -> usize {
-    input.len()
+pub fn part2(_n: &u32) -> u32 {
+    0
 }
