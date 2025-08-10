45a6b0a998bfc8e61f1c3a1757e8a6e8fe877c12
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2025-08-10 18:47:31 +0100

    solve day23 part1

--- src/year2015/day23.rs
+++ src/year2015/day23.rs
@@ -1,13 +1,100 @@
-type Input = String;
+use std::str::FromStr;
 
-pub fn parse(input: &str) -> Input {
-    input.into()
+use gxhash::{
+    HashMap,
+    HashMapExt,
+};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum Register {
+    A,
+    B,
 }
 
-pub fn part1(input: &Input) -> usize {
-    input.len()
+impl FromStr for Register {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "a" => Ok(Register::A),
+            "b" => Ok(Register::B),
+            _ => Err(()),
+        }
+    }
 }
 
-pub fn part2(input: &Input) -> usize {
+#[derive(Debug)]
+pub enum Instruction {
+    Hlf(Register),
+    Tpl(Register),
+    Inc(Register),
+    Jmp(i32),
+    Jie(Register, i32),
+    Jio(Register, i32),
+}
+
+pub fn parse(input: &str) -> Vec<Instruction> {
+    input
+        .lines()
+        .map(|line| {
+            let (instruction, remainder) = line.split_once(' ').unwrap();
+            match instruction {
+                "hlf" => Instruction::Hlf(remainder.parse().unwrap()),
+                "tpl" => Instruction::Tpl(remainder.parse().unwrap()),
+                "inc" => Instruction::Inc(remainder.parse().unwrap()),
+                "jmp" => Instruction::Jmp(remainder.parse().unwrap()),
+                "jie" => {
+                    let (register, offset) = remainder.split_once(", ").unwrap();
+                    Instruction::Jie(register.parse().unwrap(), offset.parse().unwrap())
+                }
+                "jio" => {
+                    let (register, offset) = remainder.split_once(", ").unwrap();
+                    Instruction::Jio(register.parse().unwrap(), offset.parse().unwrap())
+                }
+                _ => unreachable!(),
+            }
+        })
+        .collect()
+}
+
+pub fn part1(instructions: &[Instruction]) -> i32 {
+    let mut registers: HashMap<Register, i32> = HashMap::new();
+    let n = instructions.len();
+    let mut pc = 0i32;
+    while 0 <= pc && pc < n as i32 {
+        match instructions[pc as usize] {
+            Instruction::Hlf(register) => {
+                *registers.entry(register).or_insert(0) /= 2;
+                pc += 1;
+            }
+            Instruction::Tpl(register) => {
+                *registers.entry(register).or_insert(0) *= 3;
+                pc += 1;
+            }
+            Instruction::Inc(register) => {
+                *registers.entry(register).or_insert(0) += 1;
+                pc += 1;
+            }
+            Instruction::Jmp(offset) => pc += offset,
+            Instruction::Jie(register, offset) => {
+                if registers.get(&register).unwrap_or(&0) % 2 == 0 {
+                    pc += offset;
+                } else {
+                    pc += 1;
+                }
+            }
+            Instruction::Jio(register, offset) => {
+                if *registers.get(&register).unwrap_or(&0) == 1 {
+                    pc += offset;
+                } else {
+                    pc += 1;
+                }
+            }
+        }
+    }
+    registers[&Register::B]
+}
+
+pub fn part2(input: &[Instruction]) -> usize {
     input.len()
 }
